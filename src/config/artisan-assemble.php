73dc9e949dd9c33f48cbef622fed8a6bfbceebfa
jdw5 <joshuadwallace1@gmail.com>
2024-02-11 13:20:57 +1000

    Config variables

--- src/config/artisan-assemble.php
+++ src/config/artisan-assemble.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'frontend' => [
-        'extension' => 'vue',
-        'modal_path' => '',
-        'page_path' => '',
-    ]
+    'frontend_extension' => 'vue',
+    'modal_path' => resource_path('js/Modals'),
+    'page_path' => resource_path('js/Pages'),
+    'modal_baseroute' => 'dashboard',
+    'request_namespace' => 'App\\Http\\Requests',
 ];
